875c9384fcee804040f1c331da88fff38939a4f0
Kalyan Ghosh <155811775+Kalyane25@users.noreply.github.com>
2025-07-11 16:14:14 +0530

    Add files via upload

---
+++ countNumberOfNodes.cpp
@@ -0,0 +1,22 @@
+
+// program is to count the number of nodes in a tree 
+// code is correct but you need to write main function  and also
+// include iostream and using namespace std;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+       if(!root) return 0;
+       return 1+countNodes(root->left)+ countNodes(root->right);
+    }
+};
